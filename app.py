02c785283805af57bc2568fb8b0d323e1a8d539f
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-02-04 14:38:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ if choice == "Importar dados - CSV":
                     st.subheader("Resumo consolidado por ticker")
                     df_combo = pd.DataFrame(combos)
                     st.session_state["df_combo"] = df_combo  # guarda para reutilizar sem precisar recalcular
-                   # st.dataframe(df_combo, use_container_width=True)
+                    st.dataframe(df_combo, use_container_width=True)
 
         # Filtro e visualização persistentes do combo (não dependem de pressionar o botão novamente)
                     if "df_combo" in st.session_state:
