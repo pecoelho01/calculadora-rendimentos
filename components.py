ebf29d5afeb531bce10b0dc0381c402793bcb497
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-04-18 13:51:37 +0100

    Update components.py

--- components.py
+++ components.py
@@ -347,7 +347,7 @@ def generatePDF(total_realized,total_unrealized,total_current_value,roi_total_al
     pdf.add_page()
     pdf.set_font("Helvetica", style="B", size=16)
     pdf.cell(0,10,"Relatório da perfomance do portfólio", ln=1)
-    pdf.set_font("Helvetica", size=12)
+    pdf.set_font("Helvetica", size=10)
     pdf.cell(0, 10,f"Rentabilidade ROI : {round(roi_total_all,2)}%", ln=1)
     pdf.cell(0, 10,f"Valor atual do portfólio : {round(total_current_value, 2)}EUR", ln=1)
     pdf.cell(0,10, f"Ganhos feitos (com vendas) : {round(total_realized,2)}EUR", ln=1)
