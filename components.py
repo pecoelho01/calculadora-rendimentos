fdd9cbc47c637dd72738c3f9616e7ccb73339058
Pedro Coelho <pmnco1@iscte-iul.pt>
2026-04-18 14:44:09 +0100

    Update components.py

--- components.py
+++ components.py
@@ -346,7 +346,7 @@ def generatePDF(total_realized,total_unrealized,total_current_value,roi_total_al
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Helvetica", style="B", size=16)
-    pdf.cell(0,10,"Relatório da perfomance do portfólio", ln=1)
+    pdf.cell(0,10,"Relatório da perfomance do portfólio")
     pdf.ln()
     pdf.set_font("Helvetica", style="B", size=12)
     pdf.cell(0,10, "Dados principais", ln=1)
